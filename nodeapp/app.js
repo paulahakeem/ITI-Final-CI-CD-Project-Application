ed02b18722e673e34b2bb93101dfa2bcdebdb577
Paula <116673091+paulahakeem@users.noreply.github.com>
2023-02-25 03:25:33 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
 app.get('/', (req, res) => {
-    res.send('Hello From Osama -- Final Project ITI -- ')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+  res.send('<h1 style="color: #2196F3;">Hello From Paula 🚀</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Example app listening at http://localhost:${port}`);
+});
